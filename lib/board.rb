c16cbc5ddf3c284e3f97446153bce5af90407e07
Aaron Schreck <schreck.aaron@gmail.com>
2017-09-02 15:01:36 -0600

    Add board display method

--- lib/board.rb
+++ lib/board.rb
@@ -6,4 +6,34 @@
         def initialize 
             @cells = Array.new(4) {Array.new(4) {Cell.new}}
         end 
+    
+        def display_board
+            #I have 4 arrays that each represent a row
+            #iterate through each sub array
+            puts "==========="
+            puts ". 1 2 3 4"
+            text_array = cells.map do |row|
+                row_visual = ''
+                row.each do |cell|
+                    #case no hit 
+                    if not cell.hit
+                        row_visual = row_visual + ' '
+                    elsif cell.hit && !cell.ship
+                        row_visual = row_visual + 'M'
+                    else 
+                        row_visual = row_visual + 'H'
+                    end 
+                end 
+                
+                puts row_visual   
+            
+            end  
+            puts "==========="
+        end 
+
+        def take_a_shot(coordinates)
+
+        end 
+
+
     end 
